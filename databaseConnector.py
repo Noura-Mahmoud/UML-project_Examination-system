efd30f084cc030bb7b43f716aab440511dc145f6
Ahmed Kamal <56924629+AKreview22@users.noreply.github.com>
2023-01-04 02:06:56 +0200

    Getting students data

--- databaseConnector.py
+++ databaseConnector.py
@@ -1,11 +1,38 @@
 import pyodbc
+from prettytable import PrettyTable
 
-#connec to database
+
+#connect to database
 def connector():
     conn = pyodbc.connect(
-        "Driver={SQL Server Native Client 11.0};"
-        "server=Youssef;"
+        "Driver={SQL Server};"
+        "server=AHMEDKAMALDCFD;"
         "Database=ExaminationSystem;"
         "Trusted_Connection=yes;"
     )
     return conn
+
+#print tables in a pretty shape:
+def create_table(rows, field_names):
+    # Create a table to hold the data
+    table = PrettyTable()
+    table.field_names = field_names
+
+    # Iterate through the rows and add them to the table
+    for row in rows:
+        table.add_row(row)
+
+    return table
+
+def array_to_table(array):
+    # Get the field names from the first row of the array
+    field_names = array[0]
+
+    # Create a table with the field names
+    table = PrettyTable(field_names)
+
+    # Iterate through the rest of the rows and add them to the table
+    for row in array[1:]:
+        table.add_row(row)
+
+    return table
--- student.py
+++ student.py
@@ -1,52 +1,108 @@
-import person as p 
-from databaseConnector import connector
+import person as p
+from databaseConnector import *
+
 conn = connector()
 
 
 class Student(p.Person):
-  def __init__(self, name):
-    p.Person.__init__(self, name)
-    self.grades = dict()
-    
-  def enroll(courseID):
-    pass
-    # bussiness to add this to database with "waiting" status
-  
-  def takeExam(examID):
-    pass
-    # view exam for the student and take his answers
-    
-  def getGrades(self, stdID):
-    cursor = conn.cursor()
-    cursor.execute(
-        "GetStudentGrades ?",
-         (stdID)   
-        )
-        
-    for row in cursor:
-         print(row)
-  
-  def getApprovedCourses():
+    def __init__(self, stdID, firstName, lastName):
+        self.stdID = stdID
+        self.firstName = firstName
+        self.lastName = lastName
+
+    def enroll(self, crsName):
         cursor = conn.cursor()
-        cursor.execute(
-            "GetStudentGrades ?",
-            (stdID)   
-            )
-            
-        for row in cursor:
-            print(row)
-  
-  
-  
-    
+        cursor.execute("EXEC EnrollStudentByCourseName @stdID = ?, @crsName = ?", self.stdID, crsName)
+
+    def takeExam(examID):
+        pass
+        # view exam for the student and take his answers
+
+    def get_data(self, data_type, return_data=False):
+        cursor = conn.cursor()
+        # Execute the stored procedure
+        if data_type == "grades":
+            cursor.execute("EXEC GetStudentGrades @stdID = ?", self.stdID)
+            # Fetch all rows returned by the stored procedure
+            rows = cursor.fetchall()
+            # Create a 2D array with the rows and column values
+            field_names = ["Course Name", "Exam Description", "Grade"]
+            data = [field_names] + [[row.CrsName, row.ExamDescription, row.grade] for row in rows]
+        elif data_type == "courses":
+            cursor.execute("EXEC GetApprovedCourses @stdID = ?", self.stdID)
+            # Fetch all rows returned by the stored procedure
+            rows = cursor.fetchall()
+            # Create a 2D array with the rows and column values
+            field_names = ["Course Name"]
+            data = [field_names] + [[row.CrsName] for row in rows]
+
+        if return_data:
+            return data
+        else:
+            table = array_to_table(data)
+            print(table)
+    #
+    # def getGrades(self, stdID):
+    #     cursor = conn.cursor()
+    #     # Execute the stored procedure
+    #     cursor.execute("EXEC GetStudentGrades @stdID = ?", stdID)
+    #
+    #     # Fetch all rows returned by the stored procedure
+    #     rows = cursor.fetchall()
+    #
+    #     # Create a 2D array with the rows and column values
+    #     field_names = ["Course Name", "Exam Description", "Grade"]
+    #     grades = [field_names] + [[row.CrsName, row.ExamDescription, row.grade] for row in rows]
+    #
+    #     return grades
+    #
+    # def printGrades(self, stdID):
+    #     grades = self.getGrades(stdID)
+    #     table = array_to_table(grades)
+    #     print(table)
+    #
+    # def printApprovedCourses(self, stdID):
+    #     cursor = conn.cursor()
+    #     # Execute the stored procedure
+    #     cursor.execute("EXEC GetApprovedCourses @stdID = ?", stdID)
+    #
+    #     # Fetch all rows returned by the stored procedure
+    #     rows = cursor.fetchall()
+    #
+    #     # Create the table
+    #     field_names = ["Course Name"]
+    #     table = create_table(rows, field_names)
+    #
+    #     # Print the table
+    #     print(table)
+    #
+    #
+    # def getCourses(self, stdID):
+    #     cursor = conn.cursor()
+    #     # Execute the stored procedure
+    #     cursor.execute("EXEC GetApprovedCourses @stdID = ?", stdID)
+    #
+    #     # Fetch all rows returned by the stored procedure
+    #     rows = cursor.fetchall()
+    #
+    #     # Create a 2D array with the rows and column values
+    #     field_names = ["Course Name"]
+    #     courses = [field_names] + [[row.CrsName] for row in rows]
+    #
+    #     return courses
+    #
+    # def printCourses(self, stdID):
+    #     courses = self.getCourses(stdID)
+    #     table = array_to_table(courses)
+    #     print(table)
+
 
 #   def __str__(self):
 #     return f"{self.name}({self.age})"
 
 # p1 = Student("John", 36)
 
-# print(p1) 
+# print(p1)
 
-s1 = Student("a")
-print(s1.Name)
-s1.getGrades(2)
+s1 = Student(1,"Ahmed","Kamal")
+s1.get_data("grades")
